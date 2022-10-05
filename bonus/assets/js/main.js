316af593e683dc32aa009f5c6d8e8ef104db0a80
Pasquale Aragosa <aragosap@gmail.com>
2022-10-05 16:21:35 +0200

    add bonus

---
+++ bonus/assets/js/main.js
@@ -0,0 +1,14 @@
+// let nameUser = prompt("Inserisci il tuo nome: ");
+// console.log(nameUser);
+
+// let surname = prompt("Inserisci il tuo cognome: ");
+// console.log(surname);
+
+// const favouriteColor = prompt("Inserisci il tuo colore: ");
+// console.log(favouriteColor);
+
+// document.getElementById("my_message").innerHTML = "Password generata: " + nameUser + surname + favouriteColor + "22";
+
+let nameUser = prompt("Inserisci il tuo nome: ");
+document.getElementById("name_user").innerHTML = nameUser;
+console.log(nameUser);
