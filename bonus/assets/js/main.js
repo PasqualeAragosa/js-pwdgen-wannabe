5e9fafa7f73fc7710dd492b2c1b7f27b0c39c3ee
Pasquale Aragosa <aragosap@gmail.com>
2022-10-05 16:55:34 +0200

    add bonus style

--- bonus/assets/js/main.js
+++ bonus/assets/js/main.js
@@ -11,3 +11,11 @@
 
 let nameUser = prompt("Inserisci il tuo nome: ");
 document.getElementById("name_user").innerHTML = nameUser;
+
+let surname = prompt("Inserisci il tuo conome: ");
+document.getElementById("surname").innerHTML = surname;
+
+let favouriteColor = prompt("Inserisci il tuo colore preferito: ");
+document.getElementById("favourite_color").innerHTML = favouriteColor;
+
+document.getElementById("generator").innerHTML = nameUser + surname + favouriteColor + 22;
